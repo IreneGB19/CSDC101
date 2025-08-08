c689cc5b4ecb80339122ee9a8d33ab8d07e75156
IreneGB19 <ibalanlayos@gbox.adnu.edu.ph>
2025-08-08 19:53:24 +0800

    Update Balanlayosmain.cpp
    Updated for submission

--- Balanlayosmain.cpp
+++ Balanlayosmain.cpp
@@ -1,17 +1,16 @@
 #include <iostream> // input and output library
-#include <cmath> // for power function
 using namespace std; // standard input for cin and cout
 
 int main () {
     
-    double base1, pow1; // declared variables
+    int num1, num2, product;// declared variables
     
-    cout << "Enter a base: \n"; 
-    cin >> base1; 
-    cout << "Enter an exponent: \n"; 
-    cin >> pow1; 
-    double result = pow(base1, pow1);
-    cout << "result: " << result;
+    cout << "Enter a number: \n"; //asks user for a multiplicand
+    cin >> num1; // user input multiplicand
+    cout << "Enter another number: \n"; //asks user for a multiplier
+    cin >> num2;  // user input multiplier
+    product = num1*num2; // sets the value of product variable as the product of num1 and num2
+    cout << "The product of the two number is: " << product; // output/product
     
     return 0;
 }
